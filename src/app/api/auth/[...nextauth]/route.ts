38a3992c1a56bf54afc643a694b35495369f19dc
Whalter Duarte <dev@whalter.com.br>
2023-12-14 19:02:06 -0300

    Update next auth 05

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,62 +1,55 @@
-// Importando os módulos necessários
-import { NextApiRequest, NextApiResponse } from 'next';
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
+import NextAuth, { NextAuthOptions } from "next-auth"
+import CredentialsProvider from 'next-auth/providers/credentials'
 
-// Configurando as opções do NextAuth
-const options: NextAuthOptions = {
+export const nextAuthOptions: NextAuthOptions = {
   providers: [
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: { label: 'email', type: 'text' },
-        password: { label: 'password', type: 'password' }
-      },
-      async authorize(credentials, req) {
-        const response = await fetch(`${process.env.BASEAPI}/login`, {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json'
-          },
-          body: JSON.stringify({
-            email: credentials?.email,
-            password: credentials?.password
-          })
-        });
+   CredentialsProvider({
+     name: 'credentials',
+     credentials: {
+       email: {label: 'email', type: 'text'},
+       password: {label: 'password', type: 'password'}
+     },
 
-        const user = await response.json();
+     async authorize(credentials, req) {
+       const response = await fetch(`${process.env.BASEAPI}/login`, {
+         method: 'POST',
+         headers: {
+           'Content-type': 'application/json'
+         },
+         body: JSON.stringify({
+           email: credentials?.email,
+           password: credentials?.password
+         })
+       })
 
-        if (user && response.ok) {
-          return user;
-        }
+       const user = await response.json()
 
-        return null;
-      },
-    })
+       if (user && response.ok) {
+         return user
+       }
+
+       return null
+     },
+   })
   ],
   pages: {
-    signIn: '/blog/login'
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      user && (token.user = user);
-      return token;
-    },
-    async session({ session, token }) {
-      session = token.user as any;
-      return session;
-    }
-  }
-};
-
-// Exportando a função handler, necessária para a API Route
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  return NextAuth(req, res, options);
+   signIn: '/blog/login'
+ },
+ callbacks: {
+   async jwt({ token, user }) {
+     user && (token.user = user)
+     return token
+   },
+   async session({ session, token }){
+     session = token.user as any
+     return session
+   }
+ }
 }
 
-// Configurações adicionais para a API Route
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
+
+
+
+const handler = NextAuth(nextAuthOptions)
+
+export { handler as GET, handler as POST }
