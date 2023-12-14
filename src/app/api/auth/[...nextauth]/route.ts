7217db257dbdc5b7bdc85561f8e4cd71c372f404
Whalter Duarte <dev@whalter.com.br>
2023-12-14 17:32:22 -0300

    tsconfig update imports

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,55 +1,52 @@
-import NextAuth, { NextAuthOptions } from "next-auth"
-import CredentialsProvider from 'next-auth/providers/credentials'
+import NextAuth, { NextAuthOptions } from "next-auth";
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const nextAuthOptions: NextAuthOptions = {
   providers: [
-   CredentialsProvider({
-     name: 'credentials',
-     credentials: {
-       email: {label: 'email', type: 'text'},
-       password: {label: 'password', type: 'password'}
-     },
+    CredentialsProvider({
+      name: 'credentials',
+      credentials: {
+        email: { label: 'email', type: 'text' },
+        password: { label: 'password', type: 'password' }
+      },
 
-     async authorize(credentials, req) {
-       const response = await fetch(`${process.env.BASEAPI}/login`, {
-         method: 'POST',
-         headers: {
-           'Content-type': 'application/json'
-         },
-         body: JSON.stringify({
-           email: credentials?.email,
-           password: credentials?.password
-         })
-       })
+      async authorize(credentials, req) {
+        const response = await fetch(`${process.env.BASEAPI}/login`, {
+          method: 'POST',
+          headers: {
+            'Content-type': 'application/json'
+          },
+          body: JSON.stringify({
+            email: credentials?.email,
+            password: credentials?.password
+          })
+        });
 
-       const user = await response.json()
+        const user = await response.json();
 
-       if (user && response.ok) {
-         return user
-       }
+        if (user && response.ok) {
+          return user;
+        }
 
-       return null
-     },
-   })
+        return null;
+      },
+    })
   ],
   pages: {
-   signIn: '/blog/login'
- },
- callbacks: {
-   async jwt({ token, user }) {
-     user && (token.user = user)
-     return token
-   },
-   async session({ session, token }){
-     session = token.user as any
-     return session
-   }
- }
-}
+    signIn: '/blog/login'
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      user && (token.user = user);
+      return token;
+    },
+    async session({ session, token }) {
+      session = token.user as any;
+      return session;
+    }
+  }
+};
 
+const handler = NextAuth(nextAuthOptions);
 
-
-
-const handler = NextAuth(nextAuthOptions)
-
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
